1538bc36fc5051443c7dc353ef64057f81f53183
Ray <santosoray15@gmail.com>
2023-05-12 10:49:00 +0700

    register

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\ModelUser;
 use App\Models\User;
+use App\Models\users;
 use Illuminate\Auth\Events\Logout;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -40,27 +41,31 @@ class LoginController extends Controller
     }
 
     //register
-    public function register(Request $request)
+    public function register(Request $request, $id)
     {
         // $data['title'] = 'Register';
         // return view('user/register', $data);
 
-        $request->validate([
-            'email' => 'required',
-            'username' => 'required|unique:tb_user',
-            'jenkel'=>'required',
-            'no_telp'=>'required',
-            'password' => 'required',
-            'password_confirm' => 'required|same:password',
-        ]);
+        // $request->validate([
+        //     'email' => 'required',
+        //     'username' => 'required|unique:tb_user',
+        //     'jenkel'=>'required',
+        //     'no_telp'=>'required',
+        //     'password' => 'required',
+        //     'password_confirm' => 'required|same:password',
+        // ]);
 
-        $user = new ModelUser();
-        $user->username = $request->username;
-        $user->email = $request->email;
-        $user->jenkel = $request->jenkel;
-        $user->no_telp = $request->No_telp;
-        $user->password = bcrypt($request->password);
-        $user->save();
-        return redirect('/')->with('alert-success','Kamu berhasil Register');
+        // $user = new ModelUser();
+        // $user->username = $request->username;
+        // $user->email = $request->email;
+        // $user->jenkel = $request->jenkel;
+        // $user->no_telp = $request->No_telp;
+        // $user->password = bcrypt($request->password);
+        // $user->save();
+        // return redirect('/')->with('alert-success','Kamu berhasil Register');
+
+        users::create([
+            'id' => $request->id,
+        ]);
     }
 }
