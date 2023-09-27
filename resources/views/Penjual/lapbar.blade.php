9773a0c7e436133d642b4e0a27a78f1d21a2a42f
donat <sehahaydudidam@gmail.com>
2023-09-27 10:45:48 +0700

    Laporan belum fix

--- resources/views/Penjual/lapbar.blade.php
+++ resources/views/Penjual/lapbar.blade.php
@@ -4,6 +4,19 @@
 
 @section('content')
 
+
+<br>
+
+<section class="jumbotron text-center">
+    <div class="container">
+        <h1 class="jumbotron-heading">Laporan 1 </h1>
+    </div>
+</section>
+
+<br>
+
+
+
 <form action="/barlap" method="GET">
     @csrf
     <label for="tanggal_awal">Tanggal Awal:</label>
@@ -15,24 +28,50 @@
     <button type="submit">Tampilkan Laporan</button>
 </form>
 
-<table>
-    <thead>
-        <tr>
-            <th>Tanggal</th>
-            <th>Produk</th>
-            <th>Total Terjual</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($penjualan as $item)
-            <tr>
-                <td>{{ $item->tanggal }}</td>
-                <td>{{ $item->produk }}</td>
-                <td>{{ $item->total_terjual }}</td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
+<div class="container mb-4">
+    <div class="row">
+        <div class="col-12">
+            <div class="table-responsive">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                                                       
+                            <th scope="col">Produk</th>
+                            <th scope="col">Harga</th>
+                            <th scope="col">Terjual</th>
+                             
+
+                        </tr>
+                    </thead>
+
+                    <tbody>
+
+                         
+
+                        @foreach ($penjualan as $data)
+                            <tr>
+
+                                
+                                <td>{{ $data->produk }} </td>
+                                <td>Rp. {{ number_format($data->Harga) }} </td>
+                                <td>{{ $data->total_terjual }} </td>
+                                <td>{{ $data->Keterangan_Barang }} </td>
+                                
+
+                            </tr>
+                        @endforeach
+ 
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+ 
 
 
 @endsection
